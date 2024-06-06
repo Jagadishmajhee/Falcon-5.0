018fabdd71927ac72be5eeb229ce9c4e7cab7918
JAGADISWAR MAJHEE <jagadiswar786@gmail.com>
2024-06-06 10:39:31 +0530

    Update class 2.py

--- class 2.py
+++ class 2.py
@@ -1,13 +1,1 @@
-#Variable of python
 
-name="Sipralin"
-age=22
-salary=23000.56
-
-print(name) #Sipralin
-print(age) #22
-print(salary) #23000.50
-
-print(type(name))
-print(type(age))
-print(type(salary))
